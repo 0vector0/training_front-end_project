b3266363c1dd828480d160333bc19a47f4b5bbe1
0vector0 <0vector0@gmail.com>
2016-09-19 01:36:36 +0300

    add swiper

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
+var swiper;
 $(document).ready(function () {
 
 
     $('a').click(function () {
-
+        if (swiper != null) {
+            swiper.destroy();
+        }
         var page = $(this).attr('href');
 
         $('.pages').each(function () {
             $(this).addClass('hidden-page');
+
         });
         $(page).removeClass('hidden-page');
+        swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            effect: 'cube',
+            nextButton: '.swiper-button-next',
+            prevButton: '.swiper-button-prev',
+            slidesPerView: 1,
+            spaceBetween: 30
+            // loop: true
+        });
 
     });
+
 
 });
 
